a0e01a6c3e692681f9431155e91f14f90bf5c5ff
daemianKoh <daemian.koh@gmail.com>
2022-10-04 21:19:05 +0800

    add controller

---
+++ src/main/java/com/cwf/calculator/controller/CalculatorController.java
@@ -0,0 +1,14 @@
+package com.cwf.calculator.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+	
+	@RequestMapping(path = "/test", method = RequestMethod.GET)
+	public String createSettlements() {
+		return "HELLO WORLD";
+	}
+}
